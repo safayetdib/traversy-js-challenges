19fdfe2c33648e4af2c0d282ce5059da8f1920d0
unknown <safayetdib@gmail.com>
2023-10-26 17:10:40 +0600

    completed basic challenges 1

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+	const uniqueArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (!uniqueArr.includes(arr[i])) {
+			uniqueArr.push(arr[i]);
+		}
+	}
+	return uniqueArr;
+}
 
 module.exports = removeDuplicates;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,20 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+// 	const newArr = [];
+// 	for (i = 0; i < arr1.length; i++) {
+// 		if (arr2.includes(arr1[i]) && !newArr.includes(arr1[i]))
+// 			newArr.push(arr1[i]);
+// 	}
+// 	return newArr;
+// }
+
+function arrayIntersection(arr1, arr2) {
+	const intersectionArr = [];
+	const set1 = new Set(arr1);
+
+	for (let num of arr2) {
+		if (set1.has(num)) intersectionArr.push(num);
+	}
+	return intersectionArr;
+}
 
 module.exports = arrayIntersection;
